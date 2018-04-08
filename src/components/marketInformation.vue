b5384534630538c96285ffe3e8ec8ca5caa19e78
lishaobo <576459431qq.com>
2018-04-08 14:46:33 +0800

    第一次提交

--- src/components/marketInformation.vue
+++ src/components/marketInformation.vue
@@ -13,136 +13,26 @@
 			// 基于准备好的dom，初始化echarts实例
 			var myChart = echarts.init(document.getElementById('echartContainer'));
 			// 绘制图表
-            myChart.setOption({
-                title: {
-                    show:true,
-                    x:'center',
-                    text: '季度出访次数'
-                },
-                legend: {
-                    y:'bottom',
-                    data:['第一季度','第二季度','第三季度','第四季度'],
-
-                },
-                xAxis: {
-                    data: []
-                },
-                yAxis: {
-                    type : 'value',
-                    axisLabel:{formatter:'{value} 次'}
-
-                },
-
-                series: [
-                    {
-                        barGap:'5%',
-                        name: '第一季度',
-                        type: 'bar',
-                        data: [],
-                        itemStyle:{
-                            normal:{
-                                label:{
-                                    show:true,
-                                    position:'top',
-                                    textStyle:{
-                                        fontSize : '15',
-                                        fontFamily : '微软雅黑',
-                                    }
-                                }
-                            }
-                        },
-                    },
-                    {
-                        name: '第二季度',
-                        type: 'bar',
-                        data: [],
-                        itemStyle:{
-                            normal:{
-                                label:{
-                                    show:true,
-                                    position:'top',
-                                    textStyle:{
-                                        fontSize : '15',
-                                        fontFamily : '微软雅黑',
-                                    }
-                                }
-                            }
-                        },
-                    },
-                    {
-                        name: '第三季度',
-                        type: 'bar',
-                        data: [],
-                        itemStyle:{
-                            normal:{
-                                label:{
-                                    show:true,
-                                    position:'top',
-                                    textStyle:{
-                                        fontSize : '15',
-                                        fontFamily : '微软雅黑',
-                                    }
-                                }
-                            }
-                        },
-                    },
-                    {
-                        name: '第四季度',
-                        type: 'bar',
-                        data: [],
-                        itemStyle:{
-                            normal:{
-                                label:{
-                                    show:true,
-                                    position:'top',
-                                    textStyle:{
-                                        fontSize : '15',
-                                        fontFamily : '微软雅黑',
-                                    }
-                                }
-                            }
-                        },
-                    }
-
-                ]
-
-            });
-
-            //var contextPath = contextPath;
-
-            myChart.showLoading();
-            //请求数据
-            $.get(marketChartQuarterUrl).done(function (data) {
-                myChart.hideLoading();
-
-                myChart.setOption({
-                    xAxis: {
-                        data: data.years
-                    },
-                    series: [
-                        {
-                            name: '第一季度',
-                            data: data.firstQuarter
-                        },
-                        {
-                            name: '第二季度',
-                            data: data.secondQuarter
-                        },
-                        {
-                            name: '第三季度',
-                            data: data.thirdQuarter
-                        },
-                        {
-                            name: '第四季度',
-                            data: data.fourthQuarter
-                        },
-                    ]
-                });
-            });
-
-        
+	            var option = {
+	            title: {
+	                text: 'ECharts 入门示例'
+	            },
+	            tooltip: {},
+	            legend: {
+	                data:['销量']
+	            },
+	            xAxis: {
+	                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+	            },
+	            yAxis: {},
+	            series: [{
+	                name: '销量',
+	                type: 'bar',
+	                data: [5, 20, 36, 10, 10, 20]
+	            }]
+	        };
+			myChart.setOption(option);
 		}
-
 	}
 </script>
 
--- src/components/marketTracking.vue
+++ src/components/marketTracking.vue
@@ -1,4 +1,6 @@
 <template>
+	
+	<div>
 		<el-form ref="form" :model="sizeForm" label-width="80px" size="mini">
 				  	<el-form-item label="拜访对象" >
 				  	<el-input></el-input>
@@ -43,6 +45,27 @@
 			    <el-button type="primary" @click="onSubmit">查询</el-button>
 			  </el-form-item>
 	</el-form>
+	
+	
+	<!---->
+	
+		<el-table :data="tableData" border style="width: 100%">
+			<el-table-column  prop="Meetperson" label="拜访对象" width="150" height="30"></el-table-column>
+			<el-table-column  prop="zeren" label="责任人" width="150"></el-table-column>
+			<el-table-column  prop="date" label="拜访日期" sortable width="150"></el-table-column>
+			<el-table-column  prop="worktype" label="工作任务类型" width="150"></el-table-column>
+			<el-table-column  prop="over" label="完成情况" width="150"></el-table-column>
+			<el-table-column  prop="suozaidiqu" label="所在地区" width="150"></el-table-column>
+			<el-table-column  prop="suozai" label="所在片区" width="150"></el-table-column>
+		    <el-table-column  label="操作" width="100">
+		      	<template slot-scope="scope">
+		        	<el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+		        	<el-button type="text" size="small">编辑</el-button>
+		      	</template>
+		    </el-table-column>
+		</el-table>
+	
+	</div>
 </template>
 
 <script>
@@ -58,7 +81,20 @@
           type: [],
           resource: '',
           desc: ''
-        }
+        },
+        tableData: [{
+        	Meetperson: '小王',
+          date: '2016-05-03',
+        }, {
+        	Meetperson: '小刘',
+          date: '2016-05-02',
+        }, {
+        	Meetperson: '小李',
+          date: '2016-05-04',
+        }, {
+        	Meetperson: '小张',
+          date: '2016-05-01',
+        }]
       };
     },
     methods: {
@@ -75,7 +111,16 @@
 	.el-form-item__content .el-select--mini{
 		float: left;
 	}
+	/*按钮左对齐*/
 	.el-button--large{
 		float: left;
+	}
+	/*第二个table的表头高度控制*/
+	.el-table__header-wrapper{
+		line-height: 0px;
+	}
+	/*表头文字居中*/
+	.cell{
+		text-align: center;
 	}
 </style>
